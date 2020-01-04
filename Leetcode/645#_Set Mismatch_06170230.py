4c521befcd753268c4af1de58c9630cebff1e63c
06170230 <55569940+06170230@users.noreply.github.com>
2020-01-05 00:34:54 +0800

    Add files via upload

---
+++ Leetcode/645#_Set Mismatch_06170230.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Solution(object):
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        hashs = [0] * len(nums)
+        missing = -1
+        for i in range(len(nums)):
+            hashs[nums[i] - 1] += 1
+        return [hashs.index(2) + 1, hashs.index(0) + 1]
+
